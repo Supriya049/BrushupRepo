1b4e4c3be61a24b4ed6aa83e4697faf91dcebf56
Supriya049 <supriyakadtu049@gmail.com>
2024-10-24 10:38:19 +0530

    new substraction file created

--- src/Substraction.java
+++ src/Substraction.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Substraction {
+    public static void main(String[] args) {
+        int a = 25;
+        int b=65;
+        int c= b-a;
+        System.out.println(c);
+
+
+    }
+
 }
