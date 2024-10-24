d57d7dbede14126204ce50c9ea09933c51be1629
Supriya049 <supriyakadtu049@gmail.com>
2024-10-24 13:12:55 +0530

    done

--- Substraction.java
+++ Substraction.java
@@ -3,6 +3,7 @@ public class Substraction {
         int a=20;
         int b= 30;
         int c=a-b;
-        System.out.println(ct);
+        System.out.println(c);
+        System.out.println();
     }
 }
